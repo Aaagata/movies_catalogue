d4e064c68346d6a5b9de41a853d37d854f1634cf
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-04-06 17:16:34 +0200

    change params

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,8 +1,10 @@
 import requests, json
 
+Params = {'api_key': "b1d1fa80c18bfb8de4dc513c02c952a4"}
+
 def get_popular_movies():
     url = "https://api.themoviedb.org/3/movie/popular/"
-    params = {'api_key': "b1d1fa80c18bfb8de4dc513c02c952a4"}
+    params = Params
     response = requests.get(url, params=params)
     return response.json()
 
@@ -20,31 +22,31 @@ def get_movies(how_many, list_type):
 
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    params = {'api_key': "b1d1fa80c18bfb8de4dc513c02c952a4"}
+    params = Params
     response = requests.get(endpoint, params=params)
     return response.json()
 
 def get_single_movie_cast(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    params = {'api_key': "b1d1fa80c18bfb8de4dc513c02c952a4"}
+    params = Params
     response = requests.get(endpoint, params=params)
     return response.json()["cast"]
 
 def get_movie_images(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    params = {'api_key': "b1d1fa80c18bfb8de4dc513c02c952a4"}
+    params = Params
     response = requests.get(endpoint, params=params)
     return response.json()
 
 def get_movies_list(list_type):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    params = {'api_key': "b1d1fa80c18bfb8de4dc513c02c952a4"}
+    params = Params
     response = requests.get(endpoint, params=params)
     response.raise_for_status()
     return response.json()
 
 def get_lists():
     endpoint = "https://api.themoviedb.org/3/list/{list_id}"
-    params = {'api_key': "b1d1fa80c18bfb8de4dc513c02c952a4"}
+    params = Params
     response = requests.get(endpoint, params=params)
     return response.json()
